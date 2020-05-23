a2ea61bdfdd786a305acdc56f71d88803a071a5f
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2020-05-23 12:51:54 -0700

    added functionality to JS file. JS file is now able to display buttons and pokemon information accordingly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,43 @@
 //javascript
 
-var pokemon = "pikachu"
-var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+var pokemonArray = ["pikachu, charmander, abra"];
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-      console.log(response);
-  });
+function displayPokemonInfo() {
+    var pokemon = $(this).attr("pokemon-name");
+    var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        const pokemon = {
+            name: response.name,
+            id: response.id,
+            image: response.sprites['front_default'],
+            type: response.types.map( type => type.type.name).join(", "),
+            abilities: response.abilities.map( ability => ability.ability.name).join(", "),
+            height: response.height,
+            weight: response.weight,
+            stats: response.stats.map( stat => stat.stat.name + ' ' + stat.base_stat).join(", ")
+        };
+        console.log(pokemon);
+      });
+}
+
+function renderButtons() {
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < pokemonArray.length; i++) {
+
+        var a = $("<button>");
+        a.addClass("pokemon");
+        a.attr("pokemon-name", movies[i]);
+        a.text(pokemonArray[i]);
+        $("#buttons-view").append(a);
+    }
+}
+
+  $(document).on("click", ".pokemon", displayPokemonInfo);
+
+  renderButtons();
